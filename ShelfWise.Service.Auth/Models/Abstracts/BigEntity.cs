bc720849d8e67d8675345b459791b35bcf5396fa
Kenneth Napura <senpai24kenjie@gmail.com>
2025-07-23 23:01:24 +0800

    feature:  - Added Utility Class  - Added the Abstract Classes for the Models - Added JWT Options in AppSettings

---
+++ ShelfWise.Service.Auth/Models/Abstracts/BigEntity.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelfWise.Service.Auth.Models.Abstracts;
+
+public abstract class BigEntity
+{
+	[Key, Required] public long Id { get; set; }
+}
---
+++ ShelfWise.Service.Auth/Models/Abstracts/HashedEntity.cs
@@ -0,0 +1,12 @@
+﻿using ShelfWise.Service.Auth.AppCode.Globals;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShelfWise.Service.Auth.Models.Abstracts
+{
+	public abstract class HashedEntity
+	{
+    [Required, Key] public string Id { get; set; }
+
+    public HashedEntity(string prefix, int iteration = 4) => Id = Utils.Security.GenerateExtendedGuid(prefix, iteration);
+  }
+}
