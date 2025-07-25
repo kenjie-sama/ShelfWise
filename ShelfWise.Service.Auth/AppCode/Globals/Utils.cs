09d8a1bafc2f2d703b34266f6cb4c45ca391042d
Kenneth Napura <senpai24kenjie@gmail.com>
2025-07-25 23:28:19 +0800

    feature: - Added Utility Class.  - Added Authentication Controller  - Created Auth Db Context  - Ignored unwanted Columns  - Ignored the Migration Changes

---
+++ ShelfWise.Service.Auth/AppCode/Globals/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ShelfWise.Service.Auth.AppCode.Globals
+{
+	public static class Utils
+	{
+		public static class Urls
+		{
+			public static string Combine(string baseUrl, string relativePath)
+			{
+				var baseUri = new Uri(baseUrl, UriKind.Absolute);
+				var combinedUri = new Uri(baseUri, relativePath);
+				return combinedUri.ToString();
+			}
+
+		}
+
+		public static class Security
+		{
+      public static string GenerateExtendedGuid(string prefix, int count = 4)
+      {
+
+				if (count > 6)
+					throw new Exception("Failed to generate ID. Maximum of 6");
+
+        var result = prefix;
+        
+				for (int i = 0; i < count; i++)
+          result += Guid.NewGuid().ToString("N");
+
+        return result;
+      }
+
+			public static byte[] ConvertHexStringToBytes(string hex)
+			{
+				if (hex.Length % 2 != 0)
+					throw new ArgumentException("Invalid hex string length.");
+
+				byte[] bytes = new byte[hex.Length / 2];
+				for (int i = 0; i < hex.Length; i += 2)
+					bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+				
+				return bytes;
+			}
+
+      public static string CleanString(string value) => string.IsNullOrWhiteSpace(value) ? string.Empty : Uri.EscapeDataString(value.Trim());
+
+      public static string DecodeString(string value) => string.IsNullOrWhiteSpace(value) ? string.Empty : Uri.UnescapeDataString(value.Trim());
+
+    }
+  }
+}
---
+++ ShelfWise.Service.Auth/Context/AuthDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShelfWise.Service.Auth.Models.Entities;
+
+namespace ShelfWise.Service.Auth.Context
+{
+  public class AuthDbContext : DbContext
+  {
+    public virtual DbSet<Account> Accounts { get; set; }
+    public virtual DbSet<Person> People { get; set; }
+
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Account>()
+        .Ignore(c => c.EmailConfirmed)
+        .Ignore(c => c.PhoneNumber)
+        .Ignore(c => c.PhoneNumberConfirmed)
+        .Ignore(c => c.TwoFactorEnabled)
+        .Ignore(c => c.LockoutEnd)
+        .Ignore(c => c.AccessFailedCount)
+        .Ignore(c => c.AccessFailedCount)
+        .Ignore(c => c.SecurityStamp)
+        .Ignore(c => c.ConcurrencyStamp)
+        .Ignore(c => c.LockoutEnabled)
+        .Property(u => u.Id)
+           .HasMaxLength(250);
+    }
+  }
+}
